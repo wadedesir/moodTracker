e2620c038939b7ac85156769dbc6058457d57387
Wade Desir <wededesir@gmail.com>
2021-03-22 16:30:49 -0400

    database update

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 // config/database.js
 module.exports = {
-    'url' : 'mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    'dbName': 'wadeMongo'
+    'url' : 'mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/moodTracker?retryWrites=true&w=majority',
+    'dbName': 'moodTrackerDB'
 };
 
 /*
-heroku config:set MONGODB_URI="mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+heroku config:set MONGODB_URI="mongodb+srv://wadeMongo:wade2021@cluster0.uusdg.mongodb.net/moodTracker?retryWrites=true&w=majority"
 */
